fe9d433f8e06e168c3a8dd500d5cafa48baefac7
BorisFeng <boris.feng@decathlon.com>
2018-11-27 07:09:14 -0800

    remove geocoding

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,8 +3,6 @@ class Sport < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :messages, dependent: :destroy
   validates :title, presence: true
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
   # acts_as_taggable_on :tags
