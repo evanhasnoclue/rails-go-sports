4f2c946b9ab8ba50e60cd222bb6fdcad1f11065e
BorisFeng <boris.feng@decathlon.com>
2018-11-29 17:25:19 -0800

    deleted pgsearch

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,13 +3,13 @@ class Sport < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :messages, dependent: :destroy
   validates :title, presence: true
-  include PgSearch
-  # pg_search_scope :search_by_title_and_category,
-  #   against: [ :title, :category ],
-  #   using: {
-  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  #   }
-  multisearchable against: [ :title, :category, :start_time, :address ]
+  # include PgSearch
+  # # pg_search_scope :search_by_title_and_category,
+  # #   against: [ :title, :category ],
+  # #   using: {
+  # #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  # #   }
+  # multisearchable against: [ :title, :category, :start_time, :address ]
 
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
