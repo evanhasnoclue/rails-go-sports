21bc5f9d523a07360ed83d011fb74cbd0420190c
kai wang <wangkai199385@126.com>
2018-12-06 11:48:13 +0800

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
       # get '/tagged', to: "events#tagged", as: :tagged
     end
   end
+
+  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
